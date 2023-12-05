db9ec20a78261b0b906cd77a09d272f08690cba0
Anastasia <anastasiafateeva2406@gmail.com>
2023-12-05 22:05:32 +0300

    Add registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css'
-import {Main} from "./pages/Main/Main";
+import {Login} from "./pages/Login/Login";
 
 function App() {
   return (
     <div className={"container"}>
-      <Main/>
+      <Login/>
     </div>
   );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,36 @@
-import './Login.css'
+import styles from './Login.module.css'
 import React from "react";
 
 export const Login = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <img src={'/backArrow.png'} alt={"back"}/>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h1>Welcome aboard!</h1>
+                    <div>Let's get you going</div>
+                </div>
+                <div className={styles.block}>
+                    <div className={styles.inputBlock}>
+                        Login
+                        <input className={styles.field} type={"text"}/>
+                    </div>
+                    <div className={styles.inputBlock}>
+                        Password
+                        <input className={styles.field} type={"password"}/>
+                    </div>
+                    <div className={styles.inputBlock}>
+                        Email address
+                        <input className={styles.field} type={"email"}/>
+                    </div>
+                </div>
+                <div className={styles.button}>
+                    <input type={"button"} className={styles.submit} value={"Sign Up"}/>
+                </div>
+            </div>
+        </div>
 
+    )
 }
